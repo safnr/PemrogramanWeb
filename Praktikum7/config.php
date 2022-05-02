5fcd67c9a8ea3798eda7a4522720fe272d363282
safnr <55081040+safnr@users.noreply.github.com>
2022-05-02 21:58:46 +0700

    Update

--- Praktikum7/config.php
+++ Praktikum7/config.php
@@ -9,7 +9,5 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
-} else {
-    alert("Success to connect to MySQL");
-}
+} 
 ?>
--- Praktikum7/createTable.php
+++ Praktikum7/createTable.php
@@ -6,5 +6,13 @@ $sql = "CREATE TABLE tb_pegawai (
     id_bt INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nama VARCHAR(200) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    isi TEXT NOT NULL");
+    isi TEXT NOT NULL
+    )";
+if (mysqli_query($conn, $sql)) {
+    echo "Table created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
