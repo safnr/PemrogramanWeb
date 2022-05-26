95c238e1e92cf4f33c562b34315e947e0473c650
safnr <55081040+safnr@users.noreply.github.com>
2022-05-26 10:53:10 +0700

    Update

--- Praktikum9/createTable.php
+++ Praktikum9/createTable.php
@@ -1,17 +1,18 @@
 <?php
-include 'koneksi.inc.php';
+include 'koneksi.inc.php'; //Memanggil file koneksi.inc.php
 
-// Create
+//Membuat tabel login
 $sql = "CREATE TABLE login (
     id INT(4) AUTO_INCREMENT NOT NULL PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
     password VARCHAR(10)
     )";
+//Mengeksekusi perintah SQL
 if (mysqli_query($conn, $sql)) {
     echo "Table kontak created successfully";
 } else {
     echo "Error creating database: " . mysqli_error($conn);
 }
-
+//Menutup koneksi
 mysqli_close($conn);
 ?>
--- Praktikum9/index.php
+++ Praktikum9/index.php
@@ -1,19 +1,20 @@
 <?php   
- session_start();  
- $conn=mysqli_connect("localhost","root","","db_login");  
- $msg="";  
- if (isset($_POST['submit'])) {  
+ session_start(); //Memulai Session
+ $conn=mysqli_connect("localhost","root","","db_login"); // Koneksi ke MySQL
+ $msg=""; //Variabel untuk pesan kesalahan
+ if (isset($_POST['submit'])) { //Jika tombol submit ditekan  
       $email=mysqli_real_escape_string($conn,$_POST['email']);  
       $password=mysqli_real_escape_string($conn,$_POST['password']);  
       $sql=mysqli_query($conn,"select * from login where username='$email' && password='$password'");  
       $num=mysqli_num_rows($sql);  
+      //Jika username dan password ada yang sesuai
       if ($num>0) {
            //jika login berhasil  
            $row=mysqli_fetch_assoc($sql);  
            $_SESSION['USER_ID']=$row['id'];  
            $_SESSION['USER_NAME']=$row['username'];  
            header("location:cetak.php");
-           //jika login gagal (username/pw tidak sesuai)
+     //jika login gagal (username/pw tidak sesuai)
       }else{  
            $msg="Mohon, Masukkan Data yang Valid!";  
       }  
@@ -24,42 +25,52 @@
  <head>  
       <meta charset="utf-8">  
       <meta name="viewport" content="width=device-width, initial-scale=1">  
-      <link rel="stylesheet" type="text/css" href="css/style.css">  
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"></link>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-      <style>
-          html {
-               margin: 20px 60px;
-          }
-          .warning {
-               color: #FF0000;
-          }     
-      </style>
-      <title>Form Login</title>  
- </head>  
- <body>  
- <div class="main">  
-      <div class="flex">  
-           <div class="content">
-                <h2 class="title">Login</h2>  
-                <form method="post" action="">  
-                     <label for="username">Username</label>  
-                     <div class="box">  
-                          <input type="text" name="email" placeholder="Username" class="form-control" required>  
-                     </div>  
-                     <label for="password">Password</label>  
-                     <div class="box">  
-                          <input type="password" name="password" placeholder="Password" class="form-control" required>  
-                     </div>  
-                     <div class="btn-box">  
-                          <input type="submit" name="submit" value="Login" class="btn submit-btn">  
-                     </div>  
-                     <div class="error">  
-                          <?php echo $msg ?>  
-                     </div>  
-                </form>  
-           </div>  
-      </div>  
- </div>  
- </body>  
- </html>  
+      <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"></link>
+      <title>Form Login</title>
+ </head>
+ <body>
+ <div class="main">
+      <div class="card">
+           <div class="card-body">
+                <h2 class="text-center">Login</h2>  
+                <form method="post" action="index.php" class="login">
+                    <table width="300" align="center" cellpadding="2" cellspacing="2">
+                         <tr>
+                              <td>
+                                   Username<input type="text" name="username" placeholder="Username" class="form-control" required>
+                              </td>
+                         </tr>
+                         <tr>
+                              <td>
+                                   Password<input type="password" name="password" placeholder="Password" class="form-control" required>
+                              </td>
+                         </tr>
+                    </table>               
+                    <table width="700" align="center" cellpadding="0" cellspacing="2">
+                         <tr>
+                              <td>                        
+                                   <div class="input">
+                                        <button name="save" class="btn" value="save">Save</button>
+                                   </div>
+                              </td>
+                              <td>                        
+                                   <div class="input">
+                                        <button type="reset" name="reset" class="btn" value="Reset">Reset</button>
+                                   </div>
+                              </td>
+                         </tr>
+                    </table>
+                    <div class="error">
+                         <!--Menampilkan pesan saat terjadi error-->
+                         <?php echo $msg ?>
+                    </div>
+                </form>
+
+                </div>
+                
+           </div>
+      </div>
+ </div>
+ </body>
+ </html>
