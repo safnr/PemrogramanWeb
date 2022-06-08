6b8d47aed7086e76823ce9db33bb74bbc6b2924a
safnr <55081040+safnr@users.noreply.github.com>
2022-06-08 19:20:24 +0700

    Update

--- Praktikum10/KuisPraktikum/HalamanBeranda.php
+++ Praktikum10/KuisPraktikum/HalamanBeranda.php
@@ -1,31 +1,33 @@
 <?php   
- session_start();  
- $conn=mysqli_connect("localhost","root","","myweb");  
- if (!isset($_SESSION['USER_ID'])) {  
-      header("location:login.php");  
-      die();  
- }  
- ?>  
- <!DOCTYPE html>  
- <html>  
- <head>  
-      <meta charset="utf-8">  
-      <meta name="viewport" content="width=device-width, initial-scale=1">  
-      <title>Halaman Beranda</title>  
-      <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"></link>
+session_start();  
+$conn=mysqli_connect("localhost","root","","myweb");  
+if (!isset($_SESSION['USER_ID'])) {  
+     header("location:login.php");  
+     die();  
+}  
+?>  
+<!DOCTYPE html>  
+<html>  
+<head>  
+     <meta charset="utf-8">  
+     <meta name="viewport" content="width=device-width, initial-scale=1">  
+     <title>Halaman Beranda</title>  
+     <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"></link>
 
-      <style>  
-           body{  
-                justify-content: space-around;  
-                font-family: 'Poppins', sans-serif;  
-           }  
-      </style>  
- </head>  
- <body>  
+     <style>  
+          body{  
+               justify-content: space-around;  
+               font-family: 'Poppins', sans-serif;  
+          }  
+     </style>  
+</head>  
+<body>  
      <div class="card">
           <div class="card-body">
-               <h2 class="text-center">Selamat Datang, <?php echo $_SESSION['USER_NAME']; ?> pada hari <?php echo date("d F Y"); ?></h3>
+               <h2 class="text-center">Selamat Datang, <?php echo $_SESSION['USER_NAME']; ?> 
+               pada hari <?php include ("formatTanggal.php "); $date=date('Y-m-d');
+               echo format_hari_tanggal($date); ?></h3>
           
           </div>
      </div>  
